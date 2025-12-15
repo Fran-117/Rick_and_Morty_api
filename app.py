c1a953d693e6a2c3c0f4392682062c95809cc134
Fran_27 <27francis3@gmail.com>
2025-12-15 16:50:46 -0600

    Ya funciona, solo hay que darle formato

--- app.py
+++ app.py
@@ -1,5 +1,26 @@
+from flask import Flask, render_template   
 import requests
 
-url = "https://rickandmortyapi.com/api/character"   
-response = requests.get(url)
+app = Flask(__name__)
+
+@app.route("/")
+
+def personajes():
+    url = "https://rickandmortyapi.com/api/character"   
+    response = requests.get(url)
+    data = response.json()
+
+    personajes = []
+    for personaje in data["results"]:
+        personajes.append({
+            "nombre": personaje["name"],
+            "imagen": personaje["image"],
+            "estado": personaje["status"]
+        })
+
+    return render_template("vista.html", personajes=personajes)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
 
