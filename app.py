4917bf6f2bf0d2ea32c67602e15a2ca878b4c168
Fran_27 <27francis3@gmail.com>
2025-12-16 15:18:47 -0600

    LISTO

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 
 def personajes_y_episodios():
     url_personaje = "https://rickandmortyapi.com/api/character"
-    url_episodio = "https://rickandmortyapi.com/api/episode"      
     response = requests.get(url_personaje)
     data = response.json()
 
@@ -17,20 +16,9 @@ def personajes_y_episodios():
             "nombre": personaje["name"],
             "imagen": personaje["image"],
             "genero": personaje["gender"],
-            "lugar": personaje["origin"]["name"],
         })
 
-    resultado = requests.get(url_episodio)
-    dato = resultado.json()
-
-    episodio = []
-    for episodio in dato["results"]:
-        episodio.append({
-            "titulo": episodio["name"],
-            "fecha": episodio["air_date"],
-        })
-
-    return render_template("vista.html", personajes=personajes, episodio=episodio)
+    return render_template("vista.html", personajes=personajes)
 
 if __name__ == "__main__":
     app.run(debug=True)
