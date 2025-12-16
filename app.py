105878551960e88e88cbbb890b417c9587804803
Fran_27 <27francis3@gmail.com>
2025-12-16 14:58:20 -0600

    Agregando más datos para mi modal

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ app = Flask(__name__)
 
 @app.route("/")
 
-def personajes():
-    url = "https://rickandmortyapi.com/api/character"   
-    response = requests.get(url)
+def personajes_y_episodios():
+    url_personaje = "https://rickandmortyapi.com/api/character"
+    url_episodio = "https://rickandmortyapi.com/api/episode"      
+    response = requests.get(url_personaje)
     data = response.json()
 
     personajes = []
@@ -15,10 +16,21 @@ def personajes():
         personajes.append({
             "nombre": personaje["name"],
             "imagen": personaje["image"],
-            "estado": personaje["status"]
+            "genero": personaje["gender"],
+            "lugar": personaje["origin"]["name"],
         })
 
-    return render_template("vista.html", personajes=personajes)
+    resultado = requests.get(url_episodio)
+    dato = resultado.json()
+
+    episodio = []
+    for episodio in dato["results"]:
+        episodio.append({
+            "titulo": episodio["name"],
+            "fecha": episodio["air_date"],
+        })
+
+    return render_template("vista.html", personajes=personajes, episodio=episodio)
 
 if __name__ == "__main__":
     app.run(debug=True)
